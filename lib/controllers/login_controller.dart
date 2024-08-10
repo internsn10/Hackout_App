c9ebdabd828f20179d7adca15cab37298c281615
Vedant1130 <vedantsaparia@gmail.com>
2024-08-10 17:46:48 +0530

    "Created Login pages"

---
+++ lib/controllers/login_controller.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class LoginController extends StatefulWidget {
+  var isLoading = false.obs;
+
+  @override
+  State<LoginController> createState() => _LoginControllerState();
+}
+
+class _LoginControllerState extends State<LoginController> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
